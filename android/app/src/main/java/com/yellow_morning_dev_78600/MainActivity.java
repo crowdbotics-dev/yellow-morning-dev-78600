cfc0004877232a8329418e1e520db7725611709c
Crowdbotics <engineering@crowdbotics.com>
2023-04-24 06:34:40 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/yellow_morning_dev_78600/MainActivity.java
@@ -0,0 +1,15 @@
+package com.yellow_morning_dev_78600;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "yellow_morning_dev_78600";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "yellow-morning-dev-78600",
+  url: "https://yellow-morning-dev-78600.botics.co",
+  api: "https://yellow-morning-dev-78600.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
